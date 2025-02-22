7e6302b695ff851cfc56b8091e874e7cf4d205df
Lil-Dashik <mors_ph_sadboy@mail.ru>
2025-02-23 01:36:09 +0400

    исправила задачку 6.4.2 (без дубликата листа)

--- src/main/java/Report/MinMax.java
+++ src/main/java/Report/MinMax.java
@@ -1,27 +1,37 @@
 package Report;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
-public class MinMax {
+public class MinMax<T> {
+
     public static <T> void findMinMax(
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-        List<? extends T> list = stream.toList();
-
-        Optional<? extends T> min = list.stream().min(order);
-        Optional<? extends T> max = list.stream().max(order);
-
-        minMaxConsumer.accept(min.orElse(null), max.orElse(null));
+        Optional<List<T>> minMax = stream.reduce(
+                Optional.empty(),
+                (acc, n) -> Optional.of(Arrays.asList(
+                        acc.map(list -> order.compare(n, list.get(0)) < 0 ? n : list.get(0)).orElse(n),
+                        acc.map(list -> order.compare(n, list.get(1)) > 0 ? n : list.get(1)).orElse(n)
+                )),
+                (a, b) -> a
+        );
+        minMaxConsumer.accept(
+                minMax.map(list -> list.get(0)).orElse(null),
+                minMax.map(list -> list.get(1)).orElse(null)
+        );
     }
 
+
     public static void main(String[] args) {
-        Stream<Integer> numbers = Stream.of(5,7,9,2,56,0, -5);
+        Stream<Integer> numbers = Stream.of(5, 7, 9, 2, 56, 0, -5);
         findMinMax(numbers, Integer::compareTo, (min, max) ->
                 System.out.println("Min: " + min + ", Max: " + max));
     }
 }
+
